8b030f6c90ecebd256228e297b6a3d2896c05eab
Jenna Linh Tran <124752198+eebic@users.noreply.github.com>
2024-09-06 13:23:26 -0600

    Create ToDoList.java

---
+++ src/ToDoList/ToDoList.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ToDoList {
+    private List<Task> tasks;
+
+    public ToDoList() {
+        this.tasks = new ArrayList<>();
+    }
+
+    public void addTask(Task task) {
+        tasks.add(task);
+    }
+
+    public void removeTask(Task task) {
+        tasks.remove(task);
+    }
+
+    public List<Task> getTasks() {
+        return tasks;
+    }
+
+    public List<Task> getIncompleteTasks() {
+        List<Task> incompleteTasks = new ArrayList<>();
+        for (Task task : tasks) {
+            if (!task.isCompleted()) {
+                incompleteTasks.add(task);
+            }
+        }
+        return incompleteTasks;
+    }
+
+    public void sortTasksByDueDate() {
+        tasks.sort((t1, t2) -> t1.getDueDate().compareTo(t2.getDueDate()));
+    }
+
+    public void printTasks() {
+        for (Task task : tasks) {
+            System.out.println(task);
+        }
+    }
+}
