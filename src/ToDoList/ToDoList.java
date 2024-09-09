4df12c7be21a5945dc7e73f64b996cf26504c9ed
Jenna Linh Tran <124752198+eebic@users.noreply.github.com>
2024-09-08 23:08:28 -0600

    Update ToDoList.java

--- src/ToDoList/ToDoList.java
+++ src/ToDoList/ToDoList.java
@@ -1,42 +1,64 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class ToDoList {
-    private List<Task> tasks;
+    private List<Category> categories;
 
     public ToDoList() {
-        this.tasks = new ArrayList<>();
+        this.categories = new ArrayList<>();
     }
 
-    public void addTask(Task task) {
-        tasks.add(task);
+    public void addCategory(Category category) {
+        categories.add(category);
     }
 
-    public void removeTask(Task task) {
-        tasks.remove(task);
+    public void addTask(Task task, String categoryName) {
+        for (Category category : categories) {
+            if (category.getCategoryName().equals(categoryName)) {
+                category.addTask(task);
+                return;
+            }
+        }
+        // Create a new category if it doesn't exist
+        Category newCategory = new Category(categoryName);
+        newCategory.addTask(task);
+        categories.add(newCategory);
     }
 
     public List<Task> getTasks() {
-        return tasks;
+        return categories.stream()
+                         .flatMap(category -> category.getTasks().stream())
+                         .collect(Collectors.toList());
+    }
+
+    public List<Task> getTasksByCategory(String categoryName) {
+        return categories.stream()
+                         .filter(category -> category.getCategoryName().equals(categoryName))
+                         .flatMap(category -> category.getTasks().stream())
+                         .collect(Collectors.toList());
     }
 
     public List<Task> getIncompleteTasks() {
-        List<Task> incompleteTasks = new ArrayList<>();
-        for (Task task : tasks) {
-            if (!task.isCompleted()) {
-                incompleteTasks.add(task);
-            }
-        }
-        return incompleteTasks;
-    }
-
-    public void sortTasksByDueDate() {
-        tasks.sort((t1, t2) -> t1.getDueDate().compareTo(t2.getDueDate()));
+        return getTasks().stream()
+                         .filter(task -> !task.isCompleted())
+                         .collect(Collectors.toList());
     }
 
     public void printTasks() {
-        for (Task task : tasks) {
-            System.out.println(task);
+        getTasks().forEach(System.out::println);
+    }
+
+    public void sortTasksByDueDate() {
+        getTasks().sort((t1, t2) -> t1.getDueDate().compareTo(t2.getDueDate()));
+    }
+
+    public void printTasksByCategory() {
+        for (Category category : categories) {
+            System.out.println(category);
+            for (Task task : category.getTasks()) {
+                System.out.println("  " + task);
+            }
         }
     }
 }
